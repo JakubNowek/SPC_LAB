f592a0492ad5ed6fb81f464a0feb11b2b5378b86
JakubNowek <62256934+JakubNowek@users.noreply.github.com>
2022-01-06 21:20:03 +0100

    Complete script

--- sprawka456/spraw4.m
+++ sprawka456/spraw4.m
@@ -1,7 +1,7 @@
 clear;
 clc;
 Ts = 0.1; % sample time
-Ts_list = [0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1]; % sample time list
+Ts_list = [0.001, 0.005, 0.01, 0.05, 0.1]%, 0.2, 0.5, 1]; % sample time list
 sys_c = tf(1,[1,3,3,1]);  % continuous transfer fcn for object
 sys_d = c2d(sys_c,Ts); % discrete transfer fcn for object
 % extracting numerators and denominators
@@ -46,38 +46,48 @@ ki=0.48; % earlier estabilished minimum value of ki
 % end 
 % legend;
 %---------------------END--------------------------------%
+ki_list=0.4:0.01:0.8; %range of ki
+w=zeros(size(ki_list,2),2); %array of final values of integral of square error
 figure;
 for i= 1:size(Ts_list,2)
+    
 Ts = Ts_list(i)
-
 sys_d = c2d(sys_c,Ts); % discrete transfer fcn for object
 % extracting numerators and denominators
 [d_numerator, d_denominator] = tfdata(sys_d, 'v');
 
 %------------Q(ki) with kp and Ts as parameter-----------%
 %figure;
-ki_list=0.2:0.02:0.5;
-w=zeros(1,size(ki_list,2));
+
 for j= 1:size(ki_list,2)
 ki=ki_list(j);
 sim('z4_discPI.slx');
-txt = ['ki = ',num2str(ki)]; 
+%txt = ['ki = ',num2str(ki)]; 
 hold on
 grid on
-%plot(ans.uchyb2,'DisplayName',txt);
-w(j)=ans.uchyb2(end,1);
+%plot(ans.uchyb2,'DisplayName',txt); %error plots depending on ki
+w(j,1)=ans.uchyb2(end,1);
+w(j,2)=ki;
 end 
 %xlim([49.7 50])% ograniczanie x dla uchybów w przynliżeniu
 % legend show;
 % hold off;
-
-
-plot(ki_list,w,'-x');
+[y,x]= min(w)
+minimum = [w(x(1),2),y(1)] % smallest value of x
+txt = ['Ts = ',num2str(Ts),', min = ',num2str(minimum(2))];
+plot(w(:,2),w(:,1),'-','DisplayName',txt); % plot function 
+point = plot(minimum(1),minimum(2),'x'); % mark on the plot the min points
 hold on;
 grid on
 xlabel('ki')
 ylabel('Q(ki)')
-legend show
+point.Annotation.LegendInformation.IconDisplayStyle = 'off';
+legend;
+
+%w(:,1) - pierwsza kolumna wektora w (współrzędne y)
+%w(:,2) - druga kolumna wektora w (współrzędne x)
+
+
 %---------------------------END--------------------------%
 
 end
