c1e0fc412377dae574f0e6c33d8b9bda99d52834
JakubNowek <62256934+JakubNowek@users.noreply.github.com>
2022-01-06 16:48:50 +0100

    Major change in Ts
    We change sample time both in regulator and in object :angry:

--- sprawka456/spraw4.m
+++ sprawka456/spraw4.m
@@ -1,13 +1,12 @@
 clear;
 clc;
 Ts = 0.1; % sample time
-Ts_list = [0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1]; % sample time list
+Ts_list = [0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1,3]; % sample time list
 sys_c = tf(1,[1,3,3,1]);  % continuous transfer fcn for object
-sys_d = c2d(sys_c,Ts); % discrete transfer fcn for object
-%stepplot(sys_d);
+%sys_d = c2d(sys_c,Ts); % discrete transfer fcn for object
 % extracting numerators and denominators
 [c_numerator, c_denominator] = tfdata(sys_c, 'v');
-[d_numerator, d_denominator] = tfdata(sys_d, 'v');
+%[d_numerator, d_denominator] = tfdata(sys_d, 'v');
 
 kp=3;
 ki=0.48; % earlier estabilished minimum value of ki
@@ -35,7 +34,11 @@ figure;
 for j= 1:size(Ts_list,2)
 Ts = Ts_list(j)
 
-sim('z4_discPI.slx','StopTime','40');
+sys_d = c2d(sys_c,Ts); % discrete transfer fcn for object
+% extracting numerators and denominators
+[d_numerator, d_denominator] = tfdata(sys_d, 'v');
+
+sim('z4_discPI.slx','StopTime','400');
 txt = ['Ts = ',num2str(Ts)];
 hold on
 grid on
