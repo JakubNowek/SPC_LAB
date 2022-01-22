fdf687c3c84d14ec743aec495b5e27e948f2c29a
JakubNowek <62256934+JakubNowek@users.noreply.github.com>
2022-01-22 12:01:54 +0100

    1st method x 1st noise x const theta

--- sprawka456/zadanie5.m
+++ sprawka456/zadanie5.m
@@ -6,15 +6,16 @@
 %--------------------------------------------------------------------------
 clear all;
 clc
-theta_r = [0.6;0.8;0.5;0.9];
-N=10000;
+theta_r = [0.6;0.8;0.5;0.9]; %rzeczywiste theta
+lambda = 1;
+N=10000; %liczba probek
 U = rand(N,1)*20;
 %t = 0:1:N-1;
 %U = sin(t);
 % figure
 % plot(t,U);
 P=eye(4);
-theta = [0;0;0;0];
+theta = [0;0;0;0]; %estymowana theta
 %Y=zeros(1,N)
 Y=rand(N,1)*20;
 a1 = zeros(1,N);
@@ -24,8 +25,8 @@ b2 = zeros(1,N);
 phi = [0;0;0;0];
 for n=2:1:N  
     phi = [Y(n);Y(n-1);U(n);U(n-1)];
-    Y(n) = (phi')*theta_r+rand();%-0.5; %usrenianie szumu tym -0.5
-    P = P-(P*phi*(phi')*P)/(1+(phi')*P*phi);
+    Y(n) = (phi')*theta_r+normrnd(0,1); %normrnd(0,1) to szum o rozkladzie normalnym
+    P = (P-(P*phi*(phi')*P)/(lambda+(phi')*P*phi))/lambda;
     theta = theta+P*phi*(Y(n)-(phi')*theta);
     a1(n) = theta(1);
     a2(n) = theta(2);
@@ -40,4 +41,25 @@ plot (1:N,a1);
 plot (1:N,a2);
 plot (1:N,b1);
 plot (1:N,b2);
-%plot(1:N,Y);
+txt = ['Theta estimated with ',num2str(N),' samples']; % changing nuber to char to display it in legend
+a1=['a1*=0.6, estimated a1=',num2str(a1(n))];
+a2=['a2*=0.8, estimated a2=',num2str(a2(n))];
+b1=['b1*=0.5, estimated b1=',num2str(b1(n))];
+b2=['b2*=0.9, estimated b2=',num2str(b2(n))];
+legend(a1,a2,b1,b2,'Location','SE')
+title(txt);
+%------------------obliczanie bledu theta_r-theta-----------------%
+% figure
+% hold on;
+% grid on;
+% plot (1:N,a1-0.6);
+% plot (1:N,a2-0.8);
+% plot (1:N,b1-0.5);
+% plot (1:N,b2-0.9);
+% txt = ['Theta estimated with ',num2str(N),' samples']; % changing nuber to char to display it in legend
+% a1=['a1*=0.6, estimation error =',num2str(a1(n)-0.6)];
+% a2=['a2*=0.8, estimation error =',num2str(a2(n)-0.8)];
+% b1=['b1*=0.5, estimation error =',num2str(b1(n)-0.5)];
+% b2=['b2*=0.9, estimation error =',num2str(b2(n)-0.9)];
+% legend(a1,a2,b1,b2,'Location','SE')
+% title(txt);
