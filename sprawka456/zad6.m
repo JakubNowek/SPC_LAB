64643f83a52ac8184bc7f6b84d160114d091fc76
JakubNowek <62256934+JakubNowek@users.noreply.github.com>
2022-01-24 19:29:02 +0100

    working dataset

--- sprawka456/zad6.m
+++ sprawka456/zad6.m
@@ -21,34 +21,32 @@ eig(A) % wlasnosci wlasne, dodatnie == niestabiny
 rank(ctrb(A,B));
 
 sys=ss(A,B,C,D);
-
-Q=diag([100 1 1 1]); % jak zmienia sie system, gdy zmieniamy macierz Q oraz macierz R
+Q=diag([100 0.01 1 1]); % jak zmienia sie system, gdy zmieniamy macierz Q oraz macierz R
 R= 1; % kara, powinna byc duza
-%
 
 
 K= lqr(sys,Q,R);
 x0= [0;0;0;1];
 y0= [2;0;0;0];
 
-out=sim('zadanie6.slx');
+out=sim('zadanie6.slx','StopTime','20');%,'StopTime','100'
 %plot
-%figure('Renderer', 'painters', 'Position', [10 10 900 300]);
-figure();
+figure('Renderer', 'painters', 'Position', [10 10 1500 500]);
+%figure();
 subplot(1,2,1);
 plot(out.output.Time,out.output.Data(:,1)); hold on; grid on;
 plot(out.output.Time,out.output.Data(:,2));
 plot(out.output.Time,out.output.Data(:,3));
 plot(out.output.Time,out.output.Data(:,4));
-%title("Reakcja modelu na zadane wymuszenie dla różnych kosztów");
-%xlabel("czas [s]");
-%ylabel("Amplituda");
+title("Reakcja modelu na zadane wymuszenie dla różnych kosztów");
+xlabel("czas [s]");
+ylabel("Amplituda");
 legend({'$x$','$\dot x$','$\theta$','$\dot \theta$'},'Interpreter','latex','FontSize',14);
 subplot(1,2,2);
 plot(out.input.Time, out.input.Data);
-%title("Siła przyłożona do układu");
-%xlabel("czas [s]");
-%ylabel("Amplituda [N]");
+title("Siła przyłożona do układu");
+xlabel("czas [s]");
+ylabel("Amplituda [N]");
 grid on;
 
 
